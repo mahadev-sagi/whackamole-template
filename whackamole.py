7d4ffc6a5a2238d4d18f7c783694db7ca1877e85
Mahadev Sagi <madhsa1972@gmail.com>
2024-11-15 18:42:13 -0500

    fixed grid finished

--- whackamole.py
+++ whackamole.py
@@ -34,23 +34,31 @@ def main():
         mole_image = pygame.image.load("mole.png")
         screen = pygame.display.set_mode((640, 512))
         clock = pygame.time.Clock()
+        mole_x = 0
+        mole_y = 0
+
+
         running = True
         while running:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     running = False
                 if event.type == pygame.MOUSEBUTTONDOWN:
-                    x= event.pos[0]//32
-                    y= event.pos[1]//32
-                    print(x,y)
+                    x = event.pos[0]//32
+                    y = event.pos[1]//32
+                    if (x,y) == (mole_x//32, mole_y//32):
+                        mole_x = random.randrange(0,640,32)
+                        mole_y = random.randrange(0,512,32)
+
+
+
 
             screen.fill("light green")
             draw_grid(screen)
             pygame.draw.line(screen, "black", (640,0), (640,512), 1)
-            screen.blit(mole_image, mole_image.get_rect(topleft=(0,0)))
-
+            screen.blit(mole_image, mole_image.get_rect(topleft=(mole_x,mole_y)))
             pygame.display.flip()
-            clock.tick(60)
+            clock.tick(300)
     finally:
         pygame.quit()
 
